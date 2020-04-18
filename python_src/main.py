d3e096ca39d972c116ba3e45c63a9c405d2da1f0
curoa <yuuseitori@gmail.com>
2020-04-18 14:48:06 +0900

    change directory structure

---
+++ python_src/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+handler = logging.FileHandler(filename="log")
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)8s %(message)s'))
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+
+#logger.warn('hello warn')
+#logger.error('hello error')
+#logger.info('hello info')
+#logger.debug('hello debug')
+
+
+import argparse
+import pysnooper # @pysnooper.snoop()
+from pprint import pprint as pp
+from pprint import pformat as pf
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('This is hogehoge')
+    args = parser.parse_args()
+
+
+    print('\33[32m' + 'end' + '\033[0m')
