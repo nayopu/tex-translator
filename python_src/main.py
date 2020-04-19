1b4418a1f55a063916f7f4192b413a396ad5b4a5
teranao <terapy822@gmail.com>
2020-04-19 23:04:09 +0900

    modified main and notebook

--- python_src/main.py
+++ python_src/main.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
-
-handler = logging.FileHandler(filename="log")
-handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)8s %(message)s'))
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-
-#logger.warn('hello warn')
-#logger.error('hello error')
-#logger.info('hello info')
-#logger.debug('hello debug')
-
-
-import argparse
-import pysnooper # @pysnooper.snoop()
-from pprint import pprint as pp
-from pprint import pformat as pf
-
 import re
 import json
 
@@ -28,7 +10,7 @@ import json
 # . で任意の文字を表す
 # * で直前の文字の0回以上の連続を表す
 # .* で任意の文字列を表す
-MATH_PATTERN = '\\\\\(.*\\\\\)' # 後ろに \) があれば、できるだけ長くマッチしようとする
+# MATH_PATTERN = '\\\\\(.*\\\\\)' # 後ろに \) があれば、できるだけ長くマッチしようとする
 MATH_PATTERN = '\\\\\(.*?\\\\\)' # 最初に見つけた \) にマッチする
 
 def make_math_tag_dict(text):
@@ -67,13 +49,26 @@ def save_math_tag_dict(filename, math_tag_dict):
 def to_tag(text, jsonfile):
     math_tag_dict = make_math_tag_dict(text)
     save_math_tag_dict(jsonfile, math_tag_dict)
-    print(math_to_tag(text, math_tag_dict))
+    return math_to_tag(text, math_tag_dict)
 
 def to_math(text, jsonfile):
     math_tag_dict = load_math_tag_dict(jsonfile)
-    print(tag_to_math(text, math_tag_dict))
+    return tag_to_math(text, math_tag_dict)
 
 if __name__ == '__main__':
+    import logging
+    handler = logging.FileHandler(filename="log")
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)8s %(message)s'))
+    logger = logging.getLogger(__name__)
+    logger.addHandler(handler)
+
+    # logger.warn('hello warn')
+    # logger.error('hello error')
+    # logger.info('hello info')
+    # logger.debug('hello debug')
+
+    import argparse
+
     parser = argparse.ArgumentParser('This is hogehoge')
     parser.add_argument('command', help='2tag(t) or 2math(m)', choices=['tag', 't', 'math', 'm', ])
     parser.add_argument('filename', help='filename of tex')
